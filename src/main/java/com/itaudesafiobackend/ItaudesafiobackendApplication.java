bfc034d5bd39785f8175d57cfbe9108efe3c8eae
gilmarjusto <gilmar.justo2@gmail.com>
2025-03-27 12:02:58 -0300

    Criação do Projeto

---
+++ src/main/java/com/itaudesafiobackend/ItaudesafiobackendApplication.java
@@ -0,0 +1,13 @@
+package com.itaudesafiobackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItaudesafiobackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItaudesafiobackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/itaudesafiobackend/ItaudesafiobackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itaudesafiobackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItaudesafiobackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
